790e1417d46832c5057168056799a132721817f4
Guillermo Guerrero <william.guerrero.-nd@disney.com>
2024-12-03 01:46:08 -0500

    Add angular material

--- src/app/features/task-list/task-list.component.ts
+++ src/app/features/task-list/task-list.component.ts
@@ -2,43 +2,48 @@ import { Component, signal } from '@angular/core';
 import { TaskService } from '../../core/services/task/task.service';
 import { Task } from '../../core/interfaces/task.interface';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
     selector: 'app-task-list',
-    imports: [CommonModule],
+    imports: [CommonModule, MatSlideToggleModule, MatListModule, MatIconModule, MatButtonModule, MatCheckboxModule],
     templateUrl: './task-list.component.html',
     styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
     tasks = this.taskService.tasks;
 
-    constructor(private taskService: TaskService) {}
+    constructor(private taskService: TaskService) { }
 
     addTask() {
-      const newTask: Task = {
-        id: Date.now(),
-        title: 'Nueva tarea',
-        completed: false,
-      };
-      this.taskService.addTask(newTask);
+        const newTask: Task = {
+            id: Date.now(),
+            title: 'Nueva tarea',
+            completed: false,
+        };
+        this.taskService.addTask(newTask);
     }
 
     editTask(task: Task) {
-      const updatedTitle = prompt('Editar título de la tarea:', task.title);
-      if (updatedTitle !== null && updatedTitle.trim() !== '') {
-        const updatedTask = { ...task, title: updatedTitle };
-        this.taskService.editTask(updatedTask);
-      }
+        const updatedTitle = prompt('Editar título de la tarea:', task.title);
+        if (updatedTitle !== null && updatedTitle.trim() !== '') {
+            const updatedTask = { ...task, title: updatedTitle };
+            this.taskService.editTask(updatedTask);
+        }
     }
 
     deleteTask(taskId: number) {
-      if (confirm('¿Estás seguro de que deseas eliminar esta tarea?')) {
-        this.taskService.deleteTask(taskId);
-      }
+        if (confirm('¿Estás seguro de que deseas eliminar esta tarea?')) {
+            this.taskService.deleteTask(taskId);
+        }
     }
 
     toggleCompletion(task: Task) {
-      const updatedTask = { ...task, completed: !task.completed };
-      this.taskService.editTask(updatedTask);
+        const updatedTask = { ...task, completed: !task.completed };
+        this.taskService.editTask(updatedTask);
     }
 }
